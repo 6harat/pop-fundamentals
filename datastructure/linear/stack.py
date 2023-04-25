9574c0a7a7e67382cddbb55ba0cfa187cd1eaf7e
Bharat Gulati <bharat.gulati.certi@gmail.com>
2023-04-25 13:27:35 +0530

    onboard stack

---
+++ datastructure/linear/stack.py
@@ -0,0 +1,31 @@
+from dataclasses import dataclass, field
+
+
+@dataclass
+class Node:
+    data: int
+    next: 'Node' = field(repr=False)
+
+
+class Stack:
+    def __init__(self):
+        self._head: Node = None
+
+    def peek(self) -> int:
+        if self._head is None:
+            return None
+        return self._head.data
+
+    def pop(self) -> int:
+        if self._head is None:
+            return  None
+        node = self._head
+        self._head = node.next
+        return node.data
+
+    def push(self, data: int) -> None:
+        node = Node(data, self._head)
+        self._head = node
+
+    def is_empty(self) -> bool:
+        return self._head is None
