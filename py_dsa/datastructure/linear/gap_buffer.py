bbabe9232cfca15944a814d6ace84681bb3b2818
Bharat Gulati <bharat.gulati.certi@gmail.com>
2023-05-15 18:30:06 +0530

    onboard gap buffer

---
+++ py_dsa/datastructure/linear/gap_buffer.py
@@ -0,0 +1,50 @@
+class GapBuffer:
+
+    def __init__(self, gap_size: int = 16) -> None:
+        self._gap_size = gap_size
+        self._buffer = bytearray(self._gap_size)
+        self._gap_start = 0
+        self._gap_end = self._gap_size
+        self._n = self._gap_size
+
+    def cursor_left(self):
+        if self._gap_start == 0:
+            return
+        self._gap_start -= 1
+        self._gap_end -= 1
+        self._buffer[self._gap_end] = self._buffer[self._gap_start]
+
+    def cursor_right(self):
+        if self._gap_end == self._n:
+            return
+        self._buffer[self._gap_start] = self._buffer[self._gap_end]
+        self._gap_start += 1
+        self._gap_end += 1
+
+    def delete(self):
+        if self._gap_start == 0:
+            return
+        self._gap_start -= 1
+
+    def insert(self, char: int):
+        if self._gap_start == self._gap_end:
+            self._grow()
+        self._buffer[self._gap_start] = char
+        self._gap_start += 1
+
+    def _grow(self):
+        self._buffer.extend(bytearray(self._gap_size))
+        src_start, src_end = self._gap_end, self._gap_end
+        dst_start, dst_end = src_start + self._gap_size, src_end + self._gap_size
+        self._buffer[dst_start:dst_end] = self._buffer[src_start:src_end]
+        self._n += self._gap_size
+
+    def prettyprint(self) -> str:
+        return self._buffer[:self._gap_start].decode(
+        ) + self._buffer[self._gap_end:].decode()
+
+    def __repr__(self) -> str:
+        return f"GapBuffer(gap_size: {self._gap_size}, gap_start: {self._gap_start}, gap_end: {self._gap_end}, n: {self._n})"
+
+
+gb = GapBuffer()
