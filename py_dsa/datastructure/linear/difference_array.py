534273357882404f39ed97f3a2ef2f77dbd24719
Bharat Gulati <bharat.gulati.certi@gmail.com>
2023-05-15 11:49:58 +0530

    formatting changes

--- py_dsa/datastructure/linear/difference_array.py
+++ py_dsa/datastructure/linear/difference_array.py
@@ -8,10 +8,10 @@ class DifferenceArray:
 
     def __post_init__(self):
         self._n = len(self.values)
-        self._diffs = [0]*self._n
+        self._diffs = [0] * self._n
 
     def update(self, delta: int, start_idx: int, end_idx: int) -> None:
-        last_idx = min(end_idx, self._n-1) + 1
+        last_idx = min(end_idx, self._n - 1) + 1
         self._diffs[start_idx] += delta
         if last_idx < self._n:
             self._diffs[last_idx] -= delta
