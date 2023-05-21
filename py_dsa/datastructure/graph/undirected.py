8d8252e9f3e232f69335267203d9cbee285873c5
Bharat Gulati <bharat.gulati.certi@gmail.com>
2023-05-21 11:17:30 +0530

    onboard prim mst

--- py_dsa/datastructure/graph/undirected.py
+++ py_dsa/datastructure/graph/undirected.py
@@ -1,4 +1,7 @@
+from collections import defaultdict
+from curses import set_tabsize
 from dataclasses import dataclass
+import random
 from typing import List
 
 from py_dsa.datastructure.linear.disjoint_set import DisjointSet
@@ -19,6 +22,7 @@ class UndirectedGraph:
     def __post_init__(self):
         self._vnum = len(self.vertices)
         self._enum = len(self.edges)
+        self._random = random.Random(13)
 
     def dfs(self):
         pass
@@ -47,6 +51,41 @@ class UndirectedGraph:
         return None if midx != required_edges else mst
 
     def prim_mst(self):
+        set_tracker = DisjointSet(self.vertices)
+        sorted_edges = sorted(self.edges, key=lambda edge: edge.weight)
+        vset = {self._random.randint(0, self._vnum - 1)}
+        required_edges = self._vnum - 1
+        mst = [None] * required_edges
+        midx = 0
+        while midx < required_edges and sorted_edges:
+            eidx, edge = next((
+                (eidx, edge) for eidx, edge in enumerate(sorted_edges)
+                if (edge.source in vset or edge.target in vset) and set_tracker
+                .find_set(edge.source) != set_tracker.find_set(edge.target)),
+                              (None, None))
+            if edge is None:
+                return None
+            sorted_edges.pop(eidx)
+            vset.add(edge.source)
+            vset.add(edge.target)
+            set_tracker.union(edge.source, edge.target)
+            mst[midx] = edge
+            midx += 1
+        return None if midx != required_edges else mst
+
+    def boruvka_mst(self):
+        pass
+
+    def reverse_delete_mst(self):
+        pass
+
+    def karger_klein_tarjan_mst(self):
+        pass
+
+    def chazelle_mst(self):
+        pass
+
+    def pettie_ramachandran_mst(self):
         pass
 
 
